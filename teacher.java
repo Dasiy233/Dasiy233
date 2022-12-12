2249d37c95af119aafd53c20647da58d0ca930ef
Dasiy233 <419826967@qq.com>
2022-12-12 15:45:38 +0800

    test

---
+++ teacher.java
@@ -0,0 +1,16 @@
+//teacher 类
+
+public class teacher extends person{
+    private int wage;
+
+    public int getWage() {
+        return wage;
+    }
+
+    public void setWage(int wage) {
+        this.wage = wage;
+    }
+
+}
+
+
